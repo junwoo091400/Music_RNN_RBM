ec1b81b6aaa4540aeebb6ce587cf29f18666b052
junwoo091400 <junwoo091400@gmail.com>
2017-11-01 20:51:24 +0900

    typo in len() thing

--- rnn_rbm_generate.py
+++ rnn_rbm_generate.py
@@ -21,7 +21,7 @@ import random
 
 num = 3 #The number of songs to generate
 songsList = os.listdir('Pop_Music_Midi')
-randomSong = songsList[random.randint(0,len(randomSong)-1)]
+randomSong = songsList[random.randint(0,len(songsList)-1)]
 primer_song = 'Pop_Music_Midi' + randomSong #The path to the song to use to prime the network
 print('Primer Song = ',primer_song)
 

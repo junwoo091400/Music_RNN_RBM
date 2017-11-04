30fa2db4578f03f89610f79eee213ff9d9f13425
junwoo091400 <junwoo091400@gmail.com>
2017-11-04 16:19:35 +0900

    sdfsdfsd

--- JunwooSession.py
+++ JunwooSession.py
@@ -12,12 +12,12 @@ POP = 'Pop_Music_Midi'
 n_visible = 1
 n_hidden = 5
 
- W   = tf.Variable(tf.zeros([n_visible, n_hidden]), name="W")
- x  = tf.placeholder(tf.float32, [2, n_visible])
+W   = tf.Variable(tf.zeros([n_visible, n_hidden]), name="W")
+x  = tf.placeholder(tf.float32, [2, n_visible])
 
- bh  = tf.Variable(tf.zeros([1, n_hidden]), name="bh")
+bh  = tf.Variable(tf.zeros([1, n_hidden]), name="bh")
 
- x = tf.constant([ [1.0,2.0,3.0] ])
+x = tf.constant([ [1.0,2.0,3.0] ])
 w = tf.constant([ [2.0],[2.0],[2.0] ])
 y = tf.matmul(x,w)
 print x.get_shape()

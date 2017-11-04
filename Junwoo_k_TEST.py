2716f3bd3870b35c6b597de534dc6ab6d1184305
junwoo091400 <junwoo091400@gmail.com>
2017-11-04 19:34:24 +0900

    ipdb settrace removed

--- Junwoo_k_TEST.py
+++ Junwoo_k_TEST.py
@@ -76,7 +76,7 @@ def main():
                 out_2 = np.mean(out_2)
                 if Loss_Print_pipe.closed == False:
                     Loss_Print_pipe.write("{},{},{},{},{}\n".format(epoch, out_1, out_2 ,np.mean(costs), time.time()-start))
-                ipdb.set_trace()
+                #ipdb.set_trace()
                 print "epoch: {} out1: {} out2:{} cost: {} time: {}".format(epoch, out_1, out_2 ,np.mean(costs), time.time()-start)
                 print
                 #Here we save the weights of the model every few epochs

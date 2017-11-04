360220fd1cac157c5cf6a85d2a623421fc9fd6a8
junwoo091400 <junwoo091400@gmail.com>
2017-11-04 14:11:56 +0900

    JunwooSession test

--- JunwooSession.py
+++ JunwooSession.py
@@ -8,3 +8,12 @@ import midi_manipulation as mma
 import RBM
 import rnn_rbm_train as rrt
 POP = 'Pop_Music_Midi'
+
+# Creates a graph.
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))

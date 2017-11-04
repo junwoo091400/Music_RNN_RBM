0667d33b8eea3632fa28413f69cafaf6e9a57698
junwoo091400 <junwoo091400@gmail.com>
2017-11-04 21:35:49 +0900

    python command?

--- Junwoo_k_TEST_controller.py
+++ Junwoo_k_TEST_controller.py
@@ -4,5 +4,5 @@ import sys
 k_list = [5,10,15]
 def main(num_epoch):
 	for k in k_list:
-		os.system('Junwoo_k_TEST.py {} {}'.format(num_epoch,k))
+		os.system('python Junwoo_k_TEST.py {} {}'.format(num_epoch,k))
 main(int(sys.argv[1]))

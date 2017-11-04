1f70dd1c8d5c2215e67795c425080d9e9e319a50
junwoo091400 <junwoo091400@gmail.com>
2017-11-04 15:36:47 +0900

    Strip file extension when exporting Generated Music

--- rnn_rbm_generate.py
+++ rnn_rbm_generate.py
@@ -47,7 +47,7 @@ def main(saved_weights_path,target_dir):
         # #We generate num songs
         for i in tqdm(range(num)):
             generated_music = sess.run(generate(300), feed_dict={x: song_primer}) #Prime the network with song primer and generate an original song
-            new_song_path = "music_outputs/{}_{}".format(i, primer_song.split("/")[-1]) #The new song will be saved here
+            new_song_path = "music_outputs/{}_{}".format(i, primer_song.split('/')[-1].split('.')[0]) #The new song will be saved here
             midi_manipulation.write_song(new_song_path, generated_music)
 
 if __name__ == "__main__":

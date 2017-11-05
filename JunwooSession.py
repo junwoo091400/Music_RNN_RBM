fed7fa711e531fc73327eb19a984172586735634
junwoo091400 <junwoo091400@gmail.com>
2017-11-05 11:46:37 +0900

    Why

--- JunwooSession.py
+++ JunwooSession.py
@@ -12,7 +12,7 @@ def main(target_dir):
 	print('Generating Music from Music, just plain matrixifing,,, lol')
 	print('You Entered : *',target_dir,'*')
 	matrixified = mma.get_song(target_dir)
-	exportDir = "music_outputs/{}".foramt(target_dir.split('/')[-1].split('.')[0])
+	exportDir = "music_outputs/{}".format(target_dir.split('/')[-1].split('.')[0])
 	mma.write_song(exportDir,matrixified)
 
 main(sys.argv[1])

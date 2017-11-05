6d4bab655f9d815fa02d075a5ca99f4a46d249ac
junwoo091400 <junwoo091400@gmail.com>
2017-11-05 11:40:23 +0900

    Why

--- JunwooSession.py
+++ JunwooSession.py
@@ -10,6 +10,7 @@ import rnn_rbm_train as rrt
 
 def main(target_dir):
 	print('Generating Music from Music, just plain matrixifing,,, lol')
+	print('You Entered : *',target_dir,'*')
 	matrixified = mma.get_song(target_dir)
 	exportDir = "music_outputs/{}".foramt(target_dir.split('/')[-1].split('.')[0])
 	mma.write_song(exportDir,matrixified)
